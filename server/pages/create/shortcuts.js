c67cec5fed10f36eb2d4477a1927b5e60049cbc8
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2020-09-02 07:18:14 -0500

    Default shortcuts

---
+++ server/pages/create/shortcuts.js
@@ -0,0 +1,67 @@
+const shortcuts = [
+    {
+        name: "Show Keyboard Shortcuts",
+        default: {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            key: "q"
+        }
+    },
+    {
+        name: "Save",
+        default: {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            key: "s"
+        }
+    },
+    {
+        name: "Save As",
+        default: {
+            ctrl: true,
+            shift: true,
+            alt: false,
+            key: "s"
+        }
+    },
+    {
+        name: "Load Save",
+        default: {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            key: "o"
+        }
+    },
+    {
+        name: "Open Settings",
+        default: {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            key: "p"
+        }
+    },
+    {
+        name: "Download",
+        default: {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            key: "d"
+        }
+    },
+    {
+        name: "Upload",
+        default: {
+            ctrl: true,
+            shift: false,
+            alt: false,
+            key: "u"
+        }
+    }
+];
+
+export default shortcuts;
