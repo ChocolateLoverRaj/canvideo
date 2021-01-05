deecfa4d36be36fbbb859877945dc07840866704
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-01-05 13:04:49 -0600

    Check all video info

--- server/lib/index.ts
+++ server/lib/index.ts
@@ -9,13 +9,45 @@ server.get('/', async (req, res) => {
     res.sendStatus(503)
 })
 
-const operationsSchema = readFile(join(__dirname, '../node_modules/canvideo/dist/operations.schema.json'))
+const postSchema = (async () => ({
+    type: 'object',
+    properties: {
+        fps: {
+            type: 'number',
+            exclusiveMinimum: 0,
+            maximum: 60
+        },
+        width: {
+            type: 'number',
+            exclusiveMinimum: 0,
+            maximum: 1000,
+            multipleOf: 2
+        },
+        height: {
+            type: 'number',
+            exclusiveMinimum: 0,
+            maximum: 1000,
+            multipleOf: 2
+        },
+        frames: {
+            type: 'array',
+            items: {
+                type: 'array',
+                items: await readFile(join(__dirname, '../node_modules/canvideo/dist/operations.schema.json')),
+                maxItems: 1000
+            },
+            minItems: 1,
+            maxItems: 1000
+        }
+    },
+    required: ['fps', 'width', 'height', 'frames']
+}))()
 
 const api = Router()
-server.use('/api/v2', api)
+server.use('/api/v3', api)
 
-api.post('/', json(), validate(operationsSchema), async (req, res) => {
-    res.json(200)
+api.post('/', json(), validate(postSchema), async (req, res) => {
+    res.sendStatus(201)
 })
 
 const port = process.env.PORT || 2990
