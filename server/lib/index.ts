740f0e9c65ead6046045943a87ef8615d4663c8c
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-01-05 07:20:12 -0600

    Read operations schema

--- server/lib/index.ts
+++ server/lib/index.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import { readFile } from 'jsonfile'
+import { join } from 'path'
 
 const server = express()
 
 server.get('/', async (req, res) => {
     res.sendStatus(503)
+})
+
+const operationsSchema = readFile(join(__dirname, '../node_modules/canvideo/dist/operations.schema.json'))
+
+server.get('/api/v1', async (req, res) => {
+    res.json(await operationsSchema)
 })
 
 const port = process.env.PORT || 2990
