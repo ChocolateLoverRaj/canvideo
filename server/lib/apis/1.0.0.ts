761acd81e7738217d4252a559e4252d14fcfca47
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-01-06 08:29:13 -0600

    get video state

--- server/lib/apis/1.0.0.ts
+++ server/lib/apis/1.0.0.ts
@@ -51,21 +51,57 @@ const ensureDirs = ensureDir(generatedDir).then(async () => await Promise.all([
     emptyDir(tempDir)
 ]))
 
+// Get the state of a promise
+enum PromiseStates {
+    PENDING = 'pending',
+    RESOLVED = 'resolved',
+    REJECTED = 'rejected'
+}
+interface StateObj<T> {
+    promise: Promise<T>,
+    state: PromiseStates
+}
+const getState = <T>(promise: Promise<T>): StateObj<T> => {
+    const stateObj: StateObj<T> = {
+        promise: promise,
+        state: PromiseStates.PENDING
+    }
+    promise
+        .then(() => {
+            stateObj.state = PromiseStates.RESOLVED
+        })
+        .catch(() => {
+            stateObj.state = PromiseStates.REJECTED
+        })
+    return stateObj
+}
+
 // Map of videos
-const videos = new Map<number, Promise<void>>()
+const videos = new Map<number, StateObj<void>>()
 
 api.post('/', json(), validate(postSchema), async (req, res) => {
     const videoId = newId()
     res.status(202).json({ id: videoId })
     await ensureDirs
-    videos.set(videoId, generate(req.body.frames, {
+    videos.set(videoId, getState<void>(generate(req.body.frames, {
         fps: req.body.fps,
         width: req.body.width,
         height: req.body.height,
         outputFile: join(outputDir, `${videoId}.mp4`),
         tempDir: tempDir,
         prefix: videoId
-    }))
+    })))
+})
+
+api.get('/:id', (req, res) => {
+    const video = videos.get(parseInt(req.params.id))
+    if (!video) {
+        res.sendStatus(404)
+        return
+    }
+    res.json({
+        state: video.state
+    })
 })
 
 export default api
