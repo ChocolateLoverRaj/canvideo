a6e1b679905bf335b70d63b05b93b5187ac22039
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-01-08 21:00:12 -0600

    log process.env

--- gui/pages/index.tsx
+++ gui/pages/index.tsx
@@ -1,3 +1,12 @@
+import { GetStaticProps } from 'next'
+
 const App = (): JSX.Element => <h1>Canvideo GUI</h1>
 
 export default App
+
+export const getStaticProps: GetStaticProps = async () => {
+  console.log(process.env)
+  return {
+    props: {}
+  }
+}
