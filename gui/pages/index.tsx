42519a0f38285e42db7fa5bf510cd6a28dbcb6b7
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-01-09 07:32:00 -0600

    Figure out api route

--- gui/pages/index.tsx
+++ gui/pages/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from 'next'
 
-const App = (): JSX.Element => <h1>Canvideo GUI</h1>
+interface Props {
+  api: string
+}
+
+const App = (props: Props): JSX.Element => {
+  console.log(props.api)
+  return <h1>Canvideo GUI</h1>
+}
 
 export default App
 
-export const getStaticProps: GetStaticProps = async () => {
-  console.log(process.env)
+export const getStaticProps: GetStaticProps<Props> = async () => {
   return {
-    props: {}
+    props: {
+      api: process.env.NODE_ENV === 'production'
+        ? process.env.VERCEL_ENV === 'production'
+          ? 'https://canvideo.herokuapp.com'
+          : `https://canvideo-br-${process.env.VERCEL_GIT_COMMIT_REF}.herokuapp.com`
+        : 'localhost:2990'
+    }
   }
 }
