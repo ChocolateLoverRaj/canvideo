d1cc31b664ef244e3e21cbc2331529a6718a1a28
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-01-24 16:39:42 -0600

    project-to-operations

---
+++ package/lib/project-to-operations.ts
@@ -0,0 +1,27 @@
+import { Operations } from './operations'
+
+interface Shapes {
+  [shape: string]: (shape: any) => Operations[]
+}
+
+const shapeToOperations: Shapes = {
+  rectangle: ({ x, y, width, height }) => [['fillRect', [x, y, width, height]]],
+  coloredShape: (({ color, shape }) => [['setFillStyle', [color]], ...shapeToOperations[shape.type](shape)])
+}
+
+const projectToOperations = (project: any, fps: number): Operations[][] => {
+  const timeline = project.timeline
+  // Calculate duration
+  const duration = Math.max(...timeline.map(({ start, duration }: any) => start + duration))
+  const operations: Operations[][] = []
+  const spf = 1 / fps
+  for (let frame = 0; frame < duration * fps; frame++) {
+    const time = frame * spf
+    // Get shapes to render
+    const shapes = timeline.filter(({ start, duration }: any) => start <= time && start + duration > time)
+    operations.push(shapes.map(({ shape }: any) => shapeToOperations[shape.type](shape)).flat(1))
+  }
+  return operations
+}
+
+export default projectToOperations
