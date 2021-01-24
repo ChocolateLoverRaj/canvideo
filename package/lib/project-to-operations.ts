aa77e0caa5452aeb9376e6c76eb8503032760fe8
ChocolateLoverRaj <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-01-24 16:49:28 -0600

    package eslint

--- package/lib/project-to-operations.ts
+++ package/lib/project-to-operations.ts
@@ -6,19 +6,23 @@ interface Shapes {
 
 const shapeToOperations: Shapes = {
   rectangle: ({ x, y, width, height }) => [['fillRect', [x, y, width, height]]],
-  coloredShape: (({ color, shape }) => [['setFillStyle', [color]], ...shapeToOperations[shape.type](shape)])
+  coloredShape: ({ color, shape }) => [['setFillStyle', [color]], ...shapeToOperations[shape.type](shape)]
 }
 
 const projectToOperations = (project: any, fps: number): Operations[][] => {
   const timeline = project.timeline
   // Calculate duration
-  const duration = Math.max(...timeline.map(({ start, duration }: any) => start + duration))
+  const duration = Math.max(...timeline.map(
+    ({ start, duration }: { start: number, duration: number }) => start + duration
+  ))
   const operations: Operations[][] = []
   const spf = 1 / fps
   for (let frame = 0; frame < duration * fps; frame++) {
     const time = frame * spf
     // Get shapes to render
-    const shapes = timeline.filter(({ start, duration }: any) => start <= time && start + duration > time)
+    const shapes = timeline.filter(
+      ({ start, duration }: { start: number, duration: number }) => start <= time && start + duration > time
+    )
     operations.push(shapes.map(({ shape }: any) => shapeToOperations[shape.type](shape)).flat(1))
   }
   return operations
