1701649aaba57a47304bce8cc681e2c8a5280ac3
Rajas Paranjpe <paranjperajas@gmail.com>
2021-02-28 12:26:54 -0600

    Electron gui

---
+++ gui/electron/lib/index.ts
@@ -0,0 +1,30 @@
+import { app, BrowserWindow } from 'electron'
+import { join } from 'path'
+
+function createWindow (): void {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile(join(__dirname, '../pages/index.html')).catch(undefined)
+}
+
+app.whenReady()
+  .then(createWindow)
+  .catch(undefined)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
