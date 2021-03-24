c35f14d837c87fb4f78d95ab640e87e21e9667f2
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-24 07:25:34 -0500

    Progress bar

--- gui/pages/ffmpeg.tsx
+++ gui/pages/ffmpeg.tsx
@@ -3,6 +3,7 @@ import { createFFmpeg } from '@ffmpeg/ffmpeg'
 import never from 'never'
 import renderFrame from 'canvideo/dist/render-frame'
 import { Operations } from 'canvideo/lib/operations'
+import { Progress } from 'antd'
 
 const canvasToPng = async (canvas: HTMLCanvasElement): Promise<Uint8Array> => await new Promise((resolve, reject) => {
   canvas.toBlob(blob => {
@@ -18,9 +19,14 @@ const canvasToPng = async (canvas: HTMLCanvasElement): Promise<Uint8Array> => aw
 
 const App: FC = () => {
   const [url, setUrl] = useState<string>()
+  const [progress, setProgress] = useState(0)
 
   useEffect(() => {
-    const { load, FS, run } = createFFmpeg();
+    const { load, FS, run } = createFFmpeg({
+      progress: ({ ratio }) => {
+        setProgress(ratio)
+      }
+    });
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
     (async () => {
       console.log('loading ffmpeg')
@@ -32,7 +38,7 @@ const App: FC = () => {
       canvas.height = 300
       const ctx = canvas.getContext('2d') ?? never('no 2d')
       const frames: Operations[][] = []
-      for (let i = 0; i < 300; i++) {
+      for (let i = 0; i < 1000; i++) {
         frames.push([
           ['setFillStyle', ['white']],
           ['fillRect', [0, 0, 300, 300]],
@@ -40,13 +46,19 @@ const App: FC = () => {
           ['fillRect', [0, 0, i, i]]
         ])
       }
+      let totalSize = 0
       await Promise.all(frames.map(async (frame, index) => {
         ctx.clearRect(0, 0, 300, 300)
         renderFrame(ctx, frame)
-        FS('writeFile', `test${index}.png`, await canvasToPng(canvas))
+        const arr = await canvasToPng(canvas)
+        totalSize += arr.length
+        FS('writeFile', `test${index}.png`, arr)
       }))
-      await run('-r', '30', '-i', 'test%01d.png', '-an', '-vcodec', 'libx264', '-pix_fmt', 'yuv420p', 'output.mp4')
-      setUrl(URL.createObjectURL(new Blob([FS('readFile', 'output.mp4')], { type: 'video/mp4' })))
+      await run('-r', '60', '-i', 'test%01d.png', '-an', '-vcodec', 'libx264', '-pix_fmt', 'yuv420p', 'output.mp4')
+      const videoArr = FS('readFile', 'output.mp4')
+      console.log('frames size', totalSize)
+      console.log('video size', videoArr.length)
+      setUrl(URL.createObjectURL(new Blob([videoArr], { type: 'video/mp4' })))
 
       /* ctx.fillStyle = 'green'
       ctx.fillRect(0, 0, 200, 200)
@@ -63,13 +75,19 @@ const App: FC = () => {
     })()
   }, [])
 
+  const percent = Math.floor(progress * 100 * 100) / 100
   return url !== undefined
     ? (
       <video width={300} height={300} controls>
         <source type='video/mp4' src={url} />
       </video>
     )
-    : <>Generating video</>
+    : (
+      <>
+        Generating video
+        <Progress success={{ percent }} percent={percent} />
+      </>
+    )
 }
 
 export default App
