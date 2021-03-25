0d5f90f968307491ecc44c4d67242e8d74139000
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-25 06:59:04 -0500

    Fixed vercel.json formatting

--- gui/pages/settings.tsx
+++ gui/pages/settings.tsx
@@ -1,7 +1,29 @@
-import { FC } from 'react'
+import { FC, useCallback, useState } from 'react'
+import { Radio, RadioChangeEvent } from 'antd'
+
+enum ExportTypes {
+  MEDIA_RECORDER,
+  FFMPEG
+}
 
 const App: FC = () => {
-  return null
+  // TODO: useLocalStorage
+  const [exportType, setExportType] = useState(ExportTypes.FFMPEG)
+
+  const handleChange = useCallback(({ target: { value } }: RadioChangeEvent) => {
+    setExportType(value)
+  }, [setExportType])
+
+  return (
+    <>
+      How do you want to generate your videos?
+      <br />
+      <Radio.Group value={exportType} onChange={handleChange}>
+        <Radio value={ExportTypes.FFMPEG}>Use in browser FFmpeg</Radio>
+        <Radio value={ExportTypes.MEDIA_RECORDER}>Use MediaRecorder</Radio>
+      </Radio.Group>
+    </>
+  )
 }
 
 export default App
