0ffec10cad752e1bd51d27ea3e0820415de5267c
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-30 07:51:54 -0500

    Move next.config.js into gui

--- gui/pages/settings.tsx
+++ gui/pages/settings.tsx
@@ -1,14 +1,15 @@
-import { FC, useCallback, useState } from 'react'
+import { FC, useCallback } from 'react'
 import { Radio, RadioChangeEvent } from 'antd'
 import { ExportTypes } from '../states/Exports'
 import HeadTitle from '../components/HeadTitle'
 import mainTitle from '../lib/mainTitle'
 import exportTypes from '../lib/exportTypes'
 import exportTypeToText from '../lib/exportTypeToText'
+import { useLocalStorage } from '@rehooks/local-storage'
 
 const App: FC = () => {
   // TODO: useLocalStorage
-  const [exportType, setExportType] = useState(ExportTypes.MEDIA_RECORDER)
+  const [exportType, setExportType] = useLocalStorage('exportType', ExportTypes.WEBM_WRITER)
 
   const handleChange = useCallback(({ target: { value } }: RadioChangeEvent) => {
     setExportType(value)
@@ -19,7 +20,7 @@ const App: FC = () => {
       <HeadTitle paths={[mainTitle, 'Settings']} />
       What method do you want to use to generate your videos by default? Coming soon.
       <br />
-      <Radio.Group value={exportType} onChange={handleChange} disabled>
+      <Radio.Group value={exportType} onChange={handleChange}>
         {exportTypes.map(exportType => (
           <Radio key={exportType} value={exportType}>Use {exportTypeToText(exportType)}</Radio>
         ))}
