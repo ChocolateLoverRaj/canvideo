eef1b3734207854bdbeccc81173567c5adacb0c4
Rajas Paranjpe <paranjperajas@gmail.com>
2021-03-31 13:16:39 -0500

    More links
    Links to GitHub and Npm

--- gui/components/Header.tsx
+++ gui/components/Header.tsx
@@ -2,15 +2,21 @@ import { FC, useCallback } from 'react'
 import { useRouter } from 'next/router'
 import { Menu } from 'antd'
 import { MenuClickEventHandler } from 'rc-menu/lib/interface'
-import { PlusOutlined, DownloadOutlined, SettingOutlined } from '@ant-design/icons'
+import {
+  PlusOutlined,
+  DownloadOutlined,
+  SettingOutlined,
+  GithubOutlined
+} from '@ant-design/icons'
 import CanvideoIcon from './CanvideoIcon'
+import { ImNpm } from 'react-icons/im'
 
 const Header: FC = () => {
   const router = useRouter()
 
-  const handleClick = useCallback<MenuClickEventHandler>(e => {
+  const handleClick = useCallback<MenuClickEventHandler>(({ key }) => {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    router.replace(e.key as string)
+    if (typeof key === 'string' && key.startsWith('/')) router.replace(key)
   }, [router])
 
   return (
@@ -19,6 +25,12 @@ const Header: FC = () => {
       <Menu.Item key='/create' icon={<PlusOutlined />}>Create</Menu.Item>
       <Menu.Item key='/export' icon={<DownloadOutlined />}>Export</Menu.Item>
       <Menu.Item key='/settings' icon={<SettingOutlined />}>Settings</Menu.Item>
+      <Menu.Item icon={<GithubOutlined />}>
+        <a href='https://github.com/ChocolateLoverRaj/canvideo' target='_blank'>GitHub</a>
+      </Menu.Item>
+      <Menu.Item icon={<ImNpm />}>
+        <a href='https://npmjs.com/package/canvideo' target='_blank'>Npm</a>
+      </Menu.Item>
     </Menu>
   )
 }
