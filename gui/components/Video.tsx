a75afa63a5d10a9dc237e73f9a7adf3ffd188a60
Rajas Paranjpe <paranjperajas@gmail.com>
2021-04-02 07:05:01 -0500

    Draw frame once

--- gui/components/Video.tsx
+++ gui/components/Video.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react'
+import { FC, useEffect, useRef, useState } from 'react'
 import {
   StepBackwardOutlined,
   PauseOutlined,
@@ -7,23 +7,32 @@ import {
 import LoopIcon from './LoopIcon'
 import styles from '../styles/video.module.css'
 import ClickableProgress from './ClickableProgress'
+import never from 'never'
+
+export type RenderFn = (ctx: CanvasRenderingContext2D, t: number) => void
 
 interface Props {
   className?: string
   width: number
   height: number
   duration: number
-  // render: (ctx: CanvasRenderingContext2D, t: number) => void
+  render: RenderFn
 }
 
 const Video: FC<Props> = props => {
-  const { className, width, height } = props
+  const { className, width, height, render } = props
+
+  const ref = useRef<HTMLCanvasElement>(null)
+
+  useEffect(() => {
+    render(ref.current?.getContext('2d') ?? never('No ref or 2d'), 0)
+  }, [render, ref])
 
   const [progress, setProgress] = useState(0.5)
 
   return (
     <div className={className}>
-      <canvas width={width} height={height} />
+      <canvas width={width} height={height} ref={ref} />
       <ClickableProgress progress={progress} onChange={setProgress} />
       <div className={styles.controls}>
         <StepBackwardOutlined />
--- gui/pages/create.tsx
+++ gui/pages/create.tsx
@@ -1,20 +1,31 @@
-import { FC, useState } from 'react'
+import { FC, useCallback, useState } from 'react'
 import HeadTitle from '../components/HeadTitle'
-import Video from '../components/Video'
+import Video, { RenderFn } from '../components/Video'
 import mainTitle from '../lib/mainTitle'
 import CreateSample from '../components/CreateSample'
 import JsonInput from '@chocolateloverraj/react-json-input'
 import styles from '../styles/create.module.css'
 import projectSchema from '../lib/projectSchema'
+import renderFrame from 'canvideo/dist/render-frame'
 
 const App: FC = () => {
   const [video, setVideo] = useState<any>({
     width: 400,
     height: 400,
     duration: 10,
-    frames: []
+    frames: [
+      ['setFillStyle', ['white']],
+      ['fillRect', [0, 0, 400, 400]],
+      ['setFillStyle', ['blue']],
+      ['fillRect', [200, 200, 200, 200]]
+    ]
   })
   const { width, height, duration } = video
+
+  const render = useCallback<RenderFn>(ctx => {
+    console.log(ctx, video.frames)
+    renderFrame(ctx, video.frames)
+  }, [video.frames])
 
   return (
     <>
@@ -25,6 +36,7 @@ const App: FC = () => {
           width={width}
           height={height}
           duration={duration}
+          render={render}
         />
         <div className={styles.json}>
           <JsonInput schema={projectSchema} value={video} onChange={setVideo} />
