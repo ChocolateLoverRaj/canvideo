7c5dda47df118f080f8c82f3d35aca39b2958e23
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-12-19 09:13:38 -0800

    Add github link

--- gui/components/Header.tsx
+++ gui/components/Header.tsx
@@ -1,15 +1,17 @@
 import { FC, useCallback } from 'react'
 import { useRouter } from 'next/router'
 import { Menu, MenuProps } from 'antd'
-import { PlusOutlined, DownloadOutlined, SettingOutlined } from '@ant-design/icons'
+import { PlusOutlined, DownloadOutlined, SettingOutlined, GithubOutlined } from '@ant-design/icons'
 import CanvideoIcon from './CanvideoIcon'
 
 const Header: FC = () => {
   const router = useRouter()
 
   const handleClick = useCallback<Exclude<MenuProps['onClick'], undefined>>(e => {
+    if (e.key.startsWith('/')) {
     // eslint-disable-next-line @typescript-eslint/no-floating-promises
-    router.replace(new URL(e.key, window.location.origin))
+      router.replace(new URL(e.key, window.location.origin))
+    }
   }, [router])
 
   return (
@@ -18,6 +20,9 @@ const Header: FC = () => {
       <Menu.Item key='/create' icon={<PlusOutlined />}>Create</Menu.Item>
       <Menu.Item key='/export' icon={<DownloadOutlined />}>Export</Menu.Item>
       <Menu.Item key='/settings' icon={<SettingOutlined />}>Settings</Menu.Item>
+      <Menu.Item key='github' icon={<GithubOutlined />}>
+        <a href='https://github.com/ChocolateLoverRaj/canvideo/tree/better'>GitHub</a>
+      </Menu.Item>
     </Menu>
   )
 }
